8caaed47ebe354dbd80432e084b6622994f82188
adnans <adenansu@gmail.com>
2016-03-31 14:57:43 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool/; botRegexRules = /^\/rules/; botRegexSb = /^\/sub/; botRegexOs = /^\/opsec ;
+      botRegex = /^\/cool/; botRegexRules = /^\/rules/; botRegexSb = /^\/sub/; botRegexOs = /^\/opsec;
   
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
